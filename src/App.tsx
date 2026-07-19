488edbb1a375bf545870283e2a742dd65dd53b0c
Manaken <auriolbenoit0@gmail.com>
2026-07-19 22:42:55 +0200

    Changement de l'appel de solve

--- src/App.tsx
+++ src/App.tsx
@@ -135,9 +135,7 @@ function App() {
 
     try {
       const response = await fetch('http://localhost:8080/planning/solve', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(planningData)
+        method: 'GET'
       });
 
       if (!response.ok) {
